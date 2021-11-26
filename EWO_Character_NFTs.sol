cf96dbc0add25195cb6b38a26357dac77fb2a38e
Shantanu Sontakke <63099646+shantanhunt@users.noreply.github.com>
2021-11-26 09:18:31 +0530

    Create EWO_Character_NFTs.sol

---
+++ EWO_Character_NFTs.sol
@@ -0,0 +1,169 @@
+// contracts/MyHero.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract EWO_Character_NFT is ERC721, VRFConsumerBase, Ownable {
+    // using SafeMath for uint256;
+    using Strings for string;
+
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    uint256 public randomResult;
+    address public VRFCoordinator;
+    // rinkeby: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+    address public LinkToken;
+    // rinkeby: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709a
+
+    struct Character {
+        uint256 speed;
+        uint256 strength;
+        uint256 agility;
+        uint256 charisma;
+        uint256 power;
+        uint256 intelligence;
+        string name;
+    }
+
+    Character[] public characters;
+
+    mapping(bytes32 => string) requestToCharacterName;
+    mapping(bytes32 => address) requestToSender;
+    mapping(bytes32 => uint256) requestToTokenId;
+
+    /**
+     * Constructor inherits VRFConsumerBase
+     *
+     * Network: Rinkeby
+     * Chainlink VRF Coordinator address: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+     * LINK token address:                0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+     * Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+     */
+    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
+        // public
+        VRFConsumerBase(_VRFCoordinator, _LinkToken)
+        ERC721("EWO_Rush", "EWO")
+    {   
+        VRFCoordinator = _VRFCoordinator;
+        LinkToken = _LinkToken;
+        keyHash = _keyhash;
+        fee = 0.1 * 10**18; // 0.1 LINK
+    }
+
+    function requestNewRandomCharacter(
+        string memory name
+    ) public returns (bytes32) {
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestToCharacterName[requestId] = name;
+        requestToSender[requestId] = msg.sender;
+        return requestId;
+    }
+
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        return tokenURI(tokenId);
+    }
+
+    // function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+    //     require(
+    //         _isApprovedOrOwner(_msgSender(), tokenId),
+    //         "ERC721: transfer caller is not owner nor approved"
+    //     );
+    //     _setTokenURI(tokenId, _tokenURI);
+    // }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return "ipfs://bafyreih5kxh3ik3wzwkmm3uvkbq3ghk5xmf535sqq4hes26woc4moxwa7y/";
+    }
+
+    // function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+    //     require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+    //     string memory baseURI = _baseURI();
+    //     return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
+    // }
+
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
+        internal
+        override
+    {
+        uint256 newId = characters.length;
+        uint256 speed = (randomNumber % 100) + 1;
+        uint256 strength = ((randomNumber % 10000) / 100 ) + 1;
+        uint256 agility = ((randomNumber % 1000000) / 10000 ) + 1;
+        uint256 charisma = ((randomNumber % 100000000) / 1000000 ) + 1;
+        uint256 power = ((randomNumber % 10000000000) / 100000000 ) + 1;
+        uint256 intelligence = ((randomNumber % 1000000000000) / 10000000000) + 1;
+
+        characters.push(
+            Character(
+                speed,
+                strength,
+                agility,
+                charisma,
+                power,
+                intelligence,
+                requestToCharacterName[requestId]
+            )
+        );
+        _safeMint(requestToSender[requestId], newId);
+    }
+
+
+    function getNumberOfCharacters() public view returns (uint256) {
+        return characters.length; 
+    }
+
+    function getCharacterOverView(uint256 tokenId)
+        public
+        view
+        returns (
+            string memory,
+            uint256
+        )
+    {
+        return (
+            characters[tokenId].name,
+            characters[tokenId].speed + characters[tokenId].strength + characters[tokenId].agility + characters[tokenId].charisma + characters[tokenId].power + characters[tokenId].intelligence
+        );
+    }
+
+    function getCharacterStats(uint256 tokenId)
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256
+        )
+    {
+        return (
+            characters[tokenId].speed,
+            characters[tokenId].strength,
+            characters[tokenId].agility,
+            characters[tokenId].charisma,
+            characters[tokenId].power,
+            characters[tokenId].intelligence
+        );
+    }
+
+    // function sqrt(uint256 x) internal view returns (uint256 y) {
+    //     uint256 z = (x + 1) / 2;
+    //     y = x;
+    //     while (z < y) {
+    //         y = z;
+    //         z = (x / z + z) / 2;
+    //     }
+    // }
+}
