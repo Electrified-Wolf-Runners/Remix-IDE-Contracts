90819a1f89a2a4597b57430beddc29f4220b62cf
Shantanu Sontakke <63099646+shantanhunt@users.noreply.github.com>
2021-11-28 15:49:41 +0530

    Update EWO_Nfts.sol

--- EWO_Nfts.sol
+++ EWO_Nfts.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.3.2 (token/ERC1155/presets/ERC1155PresetMinterPauser.sol)
 
 pragma solidity ^0.8.0;
 
@@ -8,10 +7,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract EWO_Nfts is ERC1155Supply, Ownable{
-    // Obstacle ids
+    // Obstacle and land ids
     uint256 public constant GREEN_DUSTBIN = 0;
     uint256 public constant RED_BAR = 1;
     uint256 public constant BLUE_BAR = 2;
+    uint256 public constant LAND = 3;
+
     IERC20 token;
 
     // Mapping from obstacle id to token ID to owner address
@@ -23,8 +24,8 @@ contract EWO_Nfts is ERC1155Supply, Ownable{
     // Mapping from obstacle_id to obstacles sold 
     mapping (uint8 => uint256) public _sold;
 
-    // Prices of each obstacle
-    uint256[] public prices = [1000000000000000000, 1000000000000000000, 1000000000000000000];
+    // Prices of each obstacle and land
+    uint256[] public prices = [1000000000000000000, 1000000000000000000, 1000000000000000000, 1000000000000000000]; //can change price later
 
     address public feeRecipient;
     
@@ -42,17 +43,27 @@ contract EWO_Nfts is ERC1155Supply, Ownable{
         this.safeTransferFrom(address(this), owner, obstacle_id, obstacles, "");
     }
 
-    function buyObstacle(uint8 obstacleId) public payable {      //can remove payable
+    function buyObstacle(uint8 obstacleId) public {      //can remove payable
         require(obstacleId < 3, "obstacle_id does not exist");
-        require(balanceOf(msg.sender, obstacleId) < 2, "You cannot buy more NFTs");
+        // require(balanceOf(msg.sender, obstacleId) < 2, "You cannot buy more NFTs"); // comment for demonstration purposes
         uint256 price = prices[obstacleId];
-        require((token).transferFrom(msg.sender, feeRecipient, price));
         uint256 tokenid = _sold[obstacleId];
         _tokenid[obstacleId][msg.sender] = tokenid; // token id
         _owners[obstacleId][tokenid] = msg.sender;  // to keep track of owners of each tokenid
         _sold[obstacleId] += 1;
         _mint(msg.sender, obstacleId, 1, "");
-        // (nftcontract).safeTransferFrom(address(this), msg.sender, obstacleId, 1, "");
+        require((token).transferFrom(msg.sender, feeRecipient, price));
+    }
+
+    function buyLand() public {      //can remove payable
+        uint8 landId = 3;
+        uint256 price = prices[landId];
+        uint256 tokenid = _sold[landId];
+        _tokenid[landId][msg.sender] = tokenid; // token id
+        _owners[landId][tokenid] = msg.sender;  // to keep track of owners of each tokenid
+        _sold[landId] += 1;
+        _mint(msg.sender, landId, 1, "");
+        require((token).transferFrom(msg.sender, feeRecipient, price));
     }
 
     function ownerOf(uint8 obstacle_id, uint256 tokenId) public view virtual returns (address) {
@@ -60,6 +71,4 @@ contract EWO_Nfts is ERC1155Supply, Ownable{
         require(owner != address(0), "ERC1155: owner query for nonexistent token");
         return owner;
     }
-    
-    
 }
