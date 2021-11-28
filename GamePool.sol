6bc3e9084f85c5c71633a87f1476b47d3ed34b48
Shantanu Sontakke <63099646+shantanhunt@users.noreply.github.com>
2021-11-28 15:36:32 +0530

    Update GamePool.sol

--- GamePool.sol
+++ GamePool.sol
@@ -10,16 +10,38 @@ contract GamePool is Ownable{
     IERC1155 nftcontract;
     IERC20 token;
     mapping (address => uint256) public obstacleRewards;
+    mapping (address => bool) public playerFeesPaid;
     // uint256[] public rewardsPerObstacleId = [5000, 5000, 5000];
-    uint256 rewardsObstacleRate = 5000000000000000000;
 
-    constructor(address obstacleNFTAddress, address tokenAddress){
-         nftcontract = IERC1155(obstacleNFTAddress);
+    uint256 rewardsObstacleRate = 25000000000000000000;
+    uint256 entryFees = 100000000000000000000;
+    // address payable feesRecipient;
+
+    constructor(address tokenAddress){
+        //  feesRecipient = _feesRecipient;
          token = IERC20(tokenAddress);
     }
 
-    function updateObstacleRewards(address[] memory obstacleOwners) public onlyOwner(){
+    // function payFees() public payable{
+    //     require(msg.value >= _amount);
+    //     playerFeesPaid[msg.sender] = true;
+    //     (bool sent, ) =  feesRecipient.call{value: _amount}("") ;
+    //     require(sent, "Fees was not paid successfully");
+    // }
+
+    function payFees() public {
+        require((token).transferFrom(msg.sender, address(this), entryFees));
+        playerFeesPaid[msg.sender] = true;
+    }
+
+    function getContractBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    function updateObstacleRewards(address[] memory obstacleOwners) public {
         require(obstacleOwners.length <= 3);
+        require(playerFeesPaid[msg.sender] == true);
+        playerFeesPaid[msg.sender] = false;
         for(uint i = 0; i < obstacleOwners.length; i++){
             address _owner = obstacleOwners[i];
             obstacleRewards[_owner] += 1;
